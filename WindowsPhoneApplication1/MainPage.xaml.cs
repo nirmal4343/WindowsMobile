cc8b38d06ed79f458052f9286147efd903d113a0
Nirmal <nirmal.4343@gmail.com>
2014-07-18 17:08:32 -0400

    Initialize Windows Work

---
+++ WindowsPhoneApplication1/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace WindowsPhoneApplication1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            SupportedOrientations = SupportedPageOrientation.Portrait | SupportedPageOrientation.Landscape;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            WebClient twitter = new WebClient();
+            
+            twitter.DownloadStringCompleted +=new DownloadStringCompletedEventHandler(twitter_DownloadStringCompleted);
+            twitter.DownloadStringAsync(new Uri("http://learnresfull-restcall.rhcloud.com/restaurent/"));
+        }
+
+        void twitter_DownloadStringCompleted(Object sender, DownloadStringCompletedEventArgs e)
+        {
+            string jsonArrayAsString = e.Result; //"[ { Name : \"Marcos\", Country : \"Brazil\" }, { Name : \"John\", Country : \"England\" }]";
+            JArray jsonArray = JArray.Parse(jsonArrayAsString);
+            JToken jsonArray_Item = jsonArray.First;
+
+            List<Employee> employeeList = new List<Employee>();
+
+
+            while (jsonArray_Item != null)
+            {
+                string name = jsonArray_Item.Value<string>("firstName");
+                string country = jsonArray_Item.Value<string>("title");
+
+                // Be careful, you take the next from the current item, not from the JArray object.
+
+                Console.WriteLine(" ****** {0}   *******  {1}" , name,country);
+
+                employeeList.Add(new Employee
+                {
+                    EmployeeName = jsonArray_Item.Value<string>("firstName"),
+                    Title = jsonArray_Item.Value<string>("title"),
+                    Email = jsonArray_Item.Value<string>("email"),
+                    WorkPhone = jsonArray_Item.Value<string>("officePhone"),
+                    EmployeeImage = jsonArray_Item.Value<string>("imageDownloadPath")
+                });
+                jsonArray_Item = jsonArray_Item.Next;
+            }
+
+            listBox1.ItemsSource = employeeList;
+
+        }
+    }
+}
---
+++ WindowsPhoneApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WindowsPhoneApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("WindowsPhoneApplication1")]
+[assembly: AssemblyCopyright("Copyright © HP 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a38e899f-e448-4c55-a0be-3b9142a29dc6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
